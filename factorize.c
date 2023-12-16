626b557459807ddbd44fa902401dda6179e4820f
lordburaa <comrdebiruk97@gmail.com>
2023-12-15 22:19:33 -0800

    all

--- factorize.c
+++ factorize.c
@@ -1,4 +1,7 @@
 #include "fac.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
 /**
  * factorize - the function facotrize a number
  * @buffer: pointer ot the addres of the number
@@ -7,15 +10,15 @@
  */
 int factorize(char *buffer)
 {
-	int num;
-	int i;
+	uint32_t num;
+	uint32_t i;
 
 	num = atoi(buffer);
-	for (i = 2; i < num; i++)
+	for (i = 2; i < num / 2; i++)
 	{
 		if (num % i == 0)
 		{
-			printf("%d=%d=%d\n", num,num/i,i);
+			printf("%d=%d*%d\n", num,num/i,i);
 			break;
 		}
 	}
